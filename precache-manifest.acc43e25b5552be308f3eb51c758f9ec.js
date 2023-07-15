d93949983b3b8c526a1164be675600d33bdfc0b0
Iurii Kogan <koganiurii@gmail.com>
2023-07-15 10:14:50 +0300

    Updates

---
+++ precache-manifest.acc43e25b5552be308f3eb51c758f9ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3d7e6371f3cc491ca55",
+    "url": "/portfolio/static/css/main.fd2958ff.chunk.css"
+  },
+  {
+    "revision": "a3d7e6371f3cc491ca55",
+    "url": "/portfolio/static/js/main.a3d7e637.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "2b3b00c0bd0ff6a1eb9811ab3d0bc879",
+    "url": "/portfolio/index.html"
+  }
+];
